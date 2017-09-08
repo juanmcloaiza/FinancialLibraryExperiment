3f9f5cc5d041bfad5378590c5fb8b5ebc24f9721
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2017-09-08 23:45:01 +0200

    Rearanged code and corrected volatility.

--- hedging_experiment.py
+++ hedging_experiment.py
@@ -6,80 +6,93 @@ import matplotlib.pyplot as plt
 
 
 def run():
-    s0 = 1.432  # A2A.MI 2017-09-03
-    drift = 0.0
-    strike = s0
+    s0 = 10.09  # AGL.MI, 2017-04-24
+    drift = 0.0 # It is not negative
+    strike = s0 # We buy the option at the money
     expiry = 31.0  # 30 days
 
     # http://www.global-rates.com/interest-rates/libor/european-euro/eur-libor-interest-rate-overnight.aspx
-    # august 31 2017 	-0.42686 %
-    # august 30 2017 	-0.42900 %
-    # august 29 2017 	-0.42900 %
-    # august 25 2017 	-0.42543 %
-    # Libor? OIS? < 1% in any case
+    # august 31 2017    -0.42686 %
+    # august 30 2017    -0.42900 %
+    # august 29 2017    -0.42900 %
+    # august 25 2017    -0.42543 %
+    # Libor? OIS? < 1% in any case, and the price of the option is not too sensitive.
     risk_free_rate = 0.00
-    volatility = 0.16  # day-based... 1.5% ?
+    volatility = 0.0057  # day-based... 0.6% ?
     dividend = 0  # WTF should I put here?
 
-    # Generate stock price series:
+    # Generate stock price time series, Log-Normal model:
     # r = (s2 - s1) / s1 = s2/s1 - 1
-    # log(1 + r) = log(s2/s1) is normally distributed -- logrets
-    # s2 = s1*exp(logrets[2])
+    # log(r + 1) = log(s2/s1) is normally distributed -- logrets
+    # s[2] = s[1]*exp(logrets[2])
     logrets = np.random.normal(drift, volatility, 29)
     st = [s0]
     for i in range(len(logrets)):
         st += [st[-1] * np.exp(logrets[i])]
-    st = np.array(st)
+    stock = np.array(st)
 
-    # Generate option prices using Black and Scholes:
-    t = np.linspace(0, expiry - 1, len(st))
+    # Generate time series with:
+    #  - The price of the ATM call option, bought at t = 0.
+    #  - Delta of the option. 
+    #  - The value of a daily rebalanced portfolio:
+    #    A short position of the call + delta long positions of the stock. 
+    time = np.linspace(0, expiry - 1, len(stock))
     callopt = []
     deltacall = []
     portfolio = []
-    for s, ti in zip(st, t):
+    for s, ti in zip(stock, time):
         callopt += [vp.bs_call(s, ti, strike, expiry,
                                risk_free_rate, volatility, dividend)]
         deltacall += [vp.bs_call_delta(s, ti, strike, expiry,
                                        risk_free_rate, volatility, dividend)]
-        portfolio += [-callopt[-1] + deltacall[-1] * s]
-    # print(str(ti) + ",\t" + str(s) + ",\t" + str(a[-1]))
-    callopt = np.array(callopt)
+        portfolio += [ -callopt[-1] + deltacall[-1] * s]
+    callopt   = np.array(callopt)
     deltacall = np.array(deltacall)
     portfolio = np.array(portfolio)
 
-    portfolio_0 = portfolio[0]
-    portfolio_final = portfolio[-1]
-    portfolio_gain = portfolio_final / portfolio_0 - 1.0
 
-    if True:
-        print('---  STOCK ---')
-        print(st)
-        print('---  CALL  ---')
-        print(callopt)
-        print('---  DELTA ---')
-        print(deltacall)
-        print('---PORTFOLIO--')
-        print(portfolio)
-        print('--------------')
-
-        # plt.plot(t, st / st[0], 'k-')
-        # plt.plot(t, callopt / callopt[0], 'r-')
-        # plt.plot(t, portfolio / portfolio[0], 'b-')
-        # plt.show()
-    print("GAIN: ", str(portfolio_gain))
-    return portfolio_gain
+    return stock, callopt, deltacall, portfolio
 
 if __name__ == '__main__':
 
+    
+    Nruns = 120
     gains = []
-    for i in range(0, 100):
-        gains += [run()]
+    for i in range(1, Nruns+1):
+        stock, callopt, deltacall, portfolio = run();
+                
+        if True:
+            print('=== RUN {:d} ==='.format(i))
+            print('---  STOCK ---')
+            print(stock)
+            print('---  CALL  ---')
+            print(callopt)
+            print('---  DELTA ---')
+            print(deltacall)
+            print('---PORTFOLIO--')
+            print(portfolio)
+            print('--------------')
+            fig = plt.figure()
+            plt.plot(stock, 'k-')
+            plt.plot(callopt, 'r-')
+            plt.plot(portfolio, 'b-')
+            plt.savefig("{:d}.png".format(i))
+            plt.close(fig)
+        
+        gains += [(portfolio[-1] / portfolio[0]) - 1.0]
+        
+        
     gains = np.array(gains)
-    print(gains.mean())
-    print(gains.std())
-    print(gains.min())
-    print(gains.max())
+
+    print('=== GAINS ===')
+    print(gains)
+    print('=============')
+    print("Sum gains = {:.2f}".format(gains.sum()))
+    print("Average gain = {:.2f}".format(gains.mean()))
+    print("Standard dev = {:.2f}".format(gains.std()))
+    print("Minimum gain = {:.2f}".format(gains.min()))
+    print("Maximum gain = {:.2f}".format(gains.max()))
     gg = np.sort(gains)
-    print(gg[0])
-    print(gg[50])
-    print(gg[99])
+    print("Min: {:.2f}".format(gg[0]))
+    print("Median: {:.2f}".format(gg[int(Nruns/2)]))
+    print("Max: {:.2f}".format(gg[-1]))
